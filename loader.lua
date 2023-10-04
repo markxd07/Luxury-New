8ea596c2cd799acf5d41a47c013f9218f9794195
hinutt3 <116748489+markxd07@users.noreply.github.com>
2023-10-04 22:59:29 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,19 +1,21 @@
 local gamelist = {
-    [994732206]  = "", -- Check Blox Fruits Map
-    [1451439645] = "", -- Check King Legacy Map
-    [2340432463] = "", -- Check Last Pirates Map
-    [3516018619] = "", -- Check Anime Story Map
-    [2655311011] = "", -- Check Anime Diemension 
-    [1511883870] = "", -- Check Shindo Life
-    [1650291138] = "", -- Check Demon Fall
-    [380704901] = "", -- Ro-Ghoul
-    [2324662457] = "", -- AFS
-    [4730278139] = "", -- untitled boxing game
+    [994732206]  = "https://raw.githubusercontent.com/markxd07/Luxury-New/main/Blox-Fruits.lua", -- Check Blox Fruits Map
+    [1451439645] = "https://raw.githubusercontent.com/markxd07/Luxury-New/main/King-Legacy.lua", -- Check King Legacy Map
+    [2340432463] = "https://raw.githubusercontent.com/markxd07/Luxury-New/main/Last-Pirates.lua", -- Check Last Pirates Map
+    [3516018619] = "https://raw.githubusercontent.com/markxd07/Luxury-New/main/Anime-Story.lua", -- Check Anime Story Map
+    [2655311011] = "https://raw.githubusercontent.com/markxd07/Luxury-New/main/Anime-Diemension.lua", -- Check Anime Diemension 
+    [1511883870] = "https://raw.githubusercontent.com/markxd07/Luxury-New/main/Shindo-Life.lua", -- Check Shindo Life
+    [1650291138] = "https://raw.githubusercontent.com/markxd07/Luxury-New/main/Demon-Fall.lua", -- Check Demon Fall
+    [380704901] = "https://raw.githubusercontent.com/markxd07/Luxury-New/main/Ro-Ghoul.lua", -- Ro-Ghoul
+    [2324662457] = "https://raw.githubusercontent.com/markxd07/Luxury-New/main/AFS.lua", -- AFS
+    [4730278139] = "https://raw.githubusercontent.com/markxd07/Luxury-New/main/UntitledBoxingGame.lua", -- untitled boxing game
 }
+
 local checkgame = gamelist[game.gameId]
+
 if gamelist[game.gameId] then 
-print (checkgame)
+    print (checkgame)
     loadstring(game:HttpGet(checkgame))()
 else 
-game.Players.LocalPlayer:Kick("Game Not Support")
+    game.Players.LocalPlayer:Kick("Game Not Support")
 end
